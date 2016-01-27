79684dcfd6c4a1b2950080359964fd6d4d83ff02
user <maartje.holtslag@wur.nl>
2016-01-27 11:25:20 +0100

    Fifth commit: Added download to a function and create new model for Object Based Classification

---
+++ MainObjectBasedClassification.py
@@ -0,0 +1,18 @@
+from selectimage import select_image
+
+url = "https://api.planet.com/v0/scenes/ortho/"
+key = "a9dcf4c4685f46d38ef914c1fcc4c31c"
+
+nw = (-118.4, 34.084)
+se = (-118.401, 34.085)
+
+#Download the image
+select_image(url, key, nw, se)
+
+#Files
+crop_file = "/home/user/FinalAssignment/20150813_225645_0b09_visual.tif"
+in_file = "/home/user/FinalAssignment/output/InputMapObject.tif"
+
+#Cut to size
+cmd1 = "gdal_translate -srcwin 1250 1500 750 750 " + crop_file + " " + in_file
+os.system(cmd1)
---
+++ selectimage.py
@@ -0,0 +1,40 @@
+import requests
+import geojson
+from download import download_image
+
+def select_image(url, key, nw, se):
+    """Downloads an image matching the selected coordinates.
+    Args:
+        url (str): the source url of the image
+        key (str): authorisation key for the website
+        nw (lst): Decimal coordinates of the North-West corner
+        se (lst): Decimal coordinates of the South_East corner
+    Result:
+        Tif file containing the downloaded image
+    """
+    ne = (se[0], nw[1])
+    sw = (nw[0], se[1])
+    poly = geojson.Polygon([[nw, ne, se, sw, nw]])
+    intersects = geojson.dumps(poly)
+    params = {"intersects": intersects,}
+    r = requests.get(url, params=params, auth=(key, ''))
+    r.raise_for_status()
+    data = r.json()
+    scenes_data = data["features"]
+    for scene in scenes_data:
+        link = scene["properties"]["data"]["products"]["visual"]["full"]
+#        download_image(link, key)
+    
+    r = requests.get(link, stream=True, auth=(key, ''))
+    if 'content-disposition' in r.headers:
+        local_filename = r.headers['content-disposition'] \
+            .split("filename=")[-1].strip("'\"")
+    else:
+        local_filename = '.'.join(link.split('/')[-2:])
+
+    with open(local_filename, 'wb') as f:
+        for chunk in r.iter_content(chunk_size=1024):
+            if chunk: # filter out keep-alive new chunks
+                f.write(chunk)
+                f.flush()
+    return local_filename
